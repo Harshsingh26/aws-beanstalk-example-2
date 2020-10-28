347b3cb9d5862eab65ebf104123f88578f31c555
Harsh <harsh@gloify.com>
2020-10-28 06:34:56 +0530

    aws beanstalk

---
+++ src/main/java/com/techprimers/aws/awselasticbeanstalkexample2/HelloController.java
@@ -0,0 +1,10 @@
+package com.techprimers.aws.awselasticbeanstalkexample2;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+    public String hello(){return "Hello youtube"; }
+}
