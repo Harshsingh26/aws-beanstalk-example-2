63a43a3448ebd5d0c78629e97a84e8fb42978bf5
Harsh <harsh@gloify.com>
2020-10-28 11:42:20 +0530

    test changes

--- src/main/java/com/techprimers/aws/awselasticbeanstalkexample2/HelloController.java
+++ src/main/java/com/techprimers/aws/awselasticbeanstalkexample2/HelloController.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping
-    public String hello(){return "Hello youtube"; }
+    public String hello(){return "Hello youtube Gyan"; }
 }
