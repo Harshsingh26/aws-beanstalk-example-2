8c79e46f5621137e56a52ee54bf16e8aba00f971
Harsh <harsh@gloify.com>
2020-10-28 11:24:30 +0530

    added getmapping

--- src/main/java/com/techprimers/aws/awselasticbeanstalkexample2/HelloController.java
+++ src/main/java/com/techprimers/aws/awselasticbeanstalkexample2/HelloController.java
@@ -1,10 +1,12 @@
 package com.techprimers.aws.awselasticbeanstalkexample2;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
+    @GetMapping
     public String hello(){return "Hello youtube"; }
 }
